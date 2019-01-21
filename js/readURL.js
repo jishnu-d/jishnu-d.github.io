b79a5c60af7880ef99187679605ebb51c7f3bd15
Jishnu Dey <jishnudey.kgp@gmail.com>
2019-01-21 13:18:06 -0600

    cap demo

---
+++ js/readURL.js
@@ -0,0 +1,36 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#blah')
+                .attr('src', e.target.result);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$(document).ready(function() {
+    $('#submit').click(function() {
+
+        $('#blah')
+            .attr('src', $('#urlbox').val());
+
+    });
+
+    $('#gen_cap').click(function() {
+
+        console.log("http://34.73.18.246/infer?url=" + $('#blah').attr('src'));
+        $.ajax({
+            url: "http://34.73.18.246/infer?url=" + $('#blah').attr('src'),
+            type: 'GET',
+            success: function(res) {
+                console.log(res);
+                $("#caption").text(res);
+            }
+        });
+
+    });
+
+});
