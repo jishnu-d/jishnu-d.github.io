0d2f068184560a8467cf5a13ae2e3d3514d6d32e
Jishnu Dey <jishnudey.kgp@gmail.com>
2019-01-21 19:59:13 -0600

    good demo version

--- js/readURL.js
+++ js/readURL.js
@@ -16,23 +16,25 @@ $(document).ready(function() {
 
         $('#blah')
             .attr('src', $('#urlbox').val());
+        $('#gen_cap').show();
         $('#gen_cap')
             .attr('href', "http://34.73.18.246/infer?url=" + $('#blah').attr('src'));
+        
 
     });
 
-    $('#gen_cap').click(function() {
+    // $('#gen_cap').click(function() {
 
-        console.log("http://34.73.18.246/infer?url=" + $('#blah').attr('src'));
-        $.ajax({
-            url: "http://34.73.18.246/infer?url=" + $('#blah').attr('src'),
-            type: 'GET',
-            success: function(res) {
-                console.log(res);
-                $("#caption").text(res);
-            }
-        });
+    //     console.log("http://34.73.18.246/infer?url=" + $('#blah').attr('src'));
+    //     $.ajax({
+    //         url: "http://34.73.18.246/infer?url=" + $('#blah').attr('src'),
+    //         type: 'GET',
+    //         success: function(res) {
+    //             console.log(res);
+    //             $("#caption").text(res);
+    //         }
+    //     });
 
-    });
+    //});
 
 });
